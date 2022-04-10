420d430853c47d7678f1bebde2b412e0a4502d9e
Cma <chicchaima@gmail.com>
2022-04-10 13:06:08 +0900

    update repo mock, still error on test?

--- src/__tests__/unit/services/twitter.test.ts
+++ src/__tests__/unit/services/twitter.test.ts
@@ -1,10 +1,8 @@
-import { userTimeline } from '../../../repositories/mock/tweets';
+import { getMockTweetsByUser, userTimeline } from '../../../repositories/mock/tweets';
 import { getUserTimeline } from '../../../services/twitter';
 
 describe('Get users timeline', () => {
-  const mockedTimeline = getUserTimeline.inject({ getTweetsByUser: (_) => {
-    return Promise.resolve(userTimeline);
-  } });
+  const mockedTimeline = getUserTimeline.inject({ getTweetsByUser: getMockTweetsByUser});
 
   test('equals to mocked timeline.', () => {
     expect(mockedTimeline()).toEqual(Promise.resolve(userTimeline));
