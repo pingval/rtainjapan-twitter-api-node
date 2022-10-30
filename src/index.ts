15b563e087e361b65716aaffff17044ffed7bac4
Cma <chicchaima@gmail.com>
2022-10-30 13:32:32 +0900

    build: Fix build and entrypoint

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
 import app from './app';
+import http from 'http';
+import { debug } from 'console';
+
+/**
+ * Create HTTP server.
+ */
+
+const server = http.createServer(app);
+
+/**
+  * Listen on provided port, on all network interfaces.
+  */
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(
-    `rtainjapan-twitter-api-node is running at http://localhost:${port}`
-  );
+server.listen(port);
+
+server.on('listening', () => {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? `pipe ${addr}`
+    : `port ${addr?.port || 'null'}`;
+
+  debug('Listening on ' + bind);
 });
