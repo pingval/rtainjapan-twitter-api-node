d891b291bd04efec0067de629eabd5a35dca190b
Cma <chicchaima@gmail.com>
2022-11-23 16:37:39 +0900

    fix error handling for media upload

--- src/infrastructure/media.v1.ts
+++ src/infrastructure/media.v1.ts
@@ -17,7 +17,7 @@ const wrapCallTwitter = async <T>(callback: () => Promise<T>): Promise<T> => {
   } catch (e: any) {
     // eslint-disable-next-line max-len
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
-    throw new TwitterError(e.data.error || e.message);
+    throw new TwitterError(e.data?.error || e.message);
   }
 }
 export const uploadMediaV1 = async (
